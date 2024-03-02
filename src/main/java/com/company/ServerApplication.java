2862d49dd25bcb7c8f53c9a045c0afc212a79d7f
Mike Kostenko <vopros.m@gmail.com>
2024-03-02 14:31:28 +0000

    Add starting class with test, update build.gradle

--- src/main/java/com/company/ServerApplication.java
+++ src/main/java/com/company/ServerApplication.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import java.util.Stack;
-
 public class ServerApplication {
     public static void main(String[] args) {
         System.out.println("This is a first class!");
