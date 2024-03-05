97d32bf7043793e3a8066f0cfc809b5c7ffcf3ec
Mike Kostenko <vopros.m@gmail.com>
2024-03-05 14:16:39 +0000

    Implement simple server app, client app and test connection with correctness of response.

---
+++ src/main/java/com/company/ClientApplication.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * @author Mike Kostenko on 05/03/2024
+ */
+public class ClientApplication {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) {
+        try {
+            clientSocket = new Socket(ip, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            System.out.println("Exception in server: " + e);
+        }
+    }
+
+    public String sendMessage(String msg) {
+        try {
+            out.println(msg);
+            return in.readLine();
+        } catch (IOException e) {
+            System.out.println("Send message exception: " + e);
+            return e.getLocalizedMessage();
+        }
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+}
--- src/test/java/com/company/ServerApplicationTest.java
+++ src/test/java/com/company/ServerApplicationTest.java
@@ -2,12 +2,25 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * @author Mike Kostenko on 01/03/2024
  */
 public class ServerApplicationTest {
     @Test
-    public void verifyNoExceptionThrown() {
+    public void verifyNoExceptionThrown() throws IOException {
         ServerApplication.main(new String[]{});
+    }
+
+    @Test
+    public void checkResponseCorrectness() {
+        ClientApplication client = new ClientApplication();
+        client.startConnection("127.0.0.1", 4321);
+        String message = "Hello server!";
+        String response = client.sendMessage(message);
+        assertEquals(message, response);
     }
 }
