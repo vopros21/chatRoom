f86ab8f90df55ab7f8c85bdc532dfe60a0b2a7ab
Mike Kostenko <vopros.m@gmail.com>
2024-03-23 16:55:08 +0000

    Refactor ServerApplication to store and return connected usernames

--- src/main/java/com/company/ServerApplication.java
+++ src/main/java/com/company/ServerApplication.java
@@ -1,15 +1,11 @@
 package com.company;
 
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,6 +14,12 @@ public class ServerApplication {
     private ServerSocket serverSocket;
     private boolean stop;
     private final Set<ClientHandler> clients = new HashSet<>();
+    /**
+     * -- GETTER --
+     * return userNames
+     */
+    @Getter
+    private final Set<String> userNames = new HashSet<>();
 
     public static void main(String[] args) {
         ServerApplication server = new ServerApplication();
@@ -55,68 +57,18 @@ public class ServerApplication {
         }
     }
 
-    private static class ClientHandler extends Thread {
-        private final Socket clientSocket;
-        private PrintWriter writer;
-        private final ServerApplication server;
+    /**
+     * add userName to the list of online users
+     */
+    public void addUserName(String userName) {
+        userNames.add(userName);
+    }
 
-        public ClientHandler(Socket socket, ServerApplication server) {
-            log.info("Creating ClientHandler: " + this);
-            clientSocket = socket;
-            this.server = server;
-        }
-
-        public void run() {
-            try (DataInputStream in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()))) {
-                writer = new PrintWriter(clientSocket.getOutputStream(), true);
-                char dataType = in.readChar();
-                while (dataType != 0) {
-                    if (dataType == 's') {
-                        String userMsg = readString(in);
-                        if (".".equals(userMsg)) {
-                            writer.println("Tchao!");
-                            break;
-                        }
-                        server.broadcast(userMsg, this);
-                    }
-                    try {
-                        dataType = in.readChar();
-                    } catch (Exception e) {
-                        dataType = 0;
-                    }
-                }
-                clientSocket.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        private String readString(DataInputStream in) throws IOException {
-            int length = in.readInt();
-            byte[] messageByte = new byte[length];
-            boolean end = false;
-            StringBuilder dataString = new StringBuilder(length);
-            int totalBytesRead = 0;
-            while (!end) {
-                int currentsBytesRead = in.read(messageByte);
-                totalBytesRead += currentsBytesRead;
-                if (totalBytesRead <= length) {
-                    dataString.
-                            append(new String(messageByte, 0, currentsBytesRead, StandardCharsets.UTF_8));
-                } else {
-                    dataString.
-                            append(new String(messageByte, 0,
-                                    length - totalBytesRead + currentsBytesRead, StandardCharsets.UTF_8));
-                }
-                if (dataString.length() >= length) {
-                    end = true;
-                }
-            }
-            return dataString.toString();
-        }
-
-        void sendMessage(String msg) {
-            writer.println(msg);
-        }
+    /**
+     * return true if server already has connected users
+     * false otherwise
+     */
+    public boolean hasUsers() {
+        return !this.userNames.isEmpty();
     }
 }
