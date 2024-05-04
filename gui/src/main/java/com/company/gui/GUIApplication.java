935d36d8409efd4c219164e9256300c469764c44
Mike Kostenko <vopros.m@gmail.com>
2024-05-04 21:54:21 +0100

    Add UI interface, first iteration

--- gui/src/main/java/com/company/gui/GUIApplication.java
+++ gui/src/main/java/com/company/gui/GUIApplication.java
@@ -1,44 +1,79 @@
 package com.company.gui;
 
 import com.company.gui.utils.HintTextField;
-import com.company.server.Client;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GUIApplication extends JFrame {
     private JTextArea messageArea;
     private JTextField textField;
-    private Client client;
+    private JButton sendButton;
+    private JButton exitButton;
+    private JLabel connectedTo = new JLabel();
+    private ChatClient client;
+    private int serverPort = 3000;
+    private String hostname = "localhost";
 
     public GUIApplication() {
         super("chatRoom Application");
         setSize(400, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        connectedTo.setText("Connected to: %s:%d".formatted(hostname, serverPort));
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(new JSeparator());
+        menuBar.add(connectedTo);
+        setJMenuBar(menuBar);
+        menuBar.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, new Color(0, 0, 0, 0)));
+
         messageArea = new JTextArea();
         messageArea.setEditable(false);
         add(new JScrollPane(messageArea), BorderLayout.CENTER);
 
+        JPanel lowerPane = new JPanel();
+        lowerPane.setLayout(new BorderLayout());
+        lowerPane.setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, new Color(0, 0, 0, 0)));
+
         textField = new HintTextField("Type the message here...");
-        textField.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textField.setText("");
+        textField.addActionListener(e -> textField.setText(""));
+        lowerPane.add(textField, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+
+        sendButton = new JButton("Send");
+        buttonPanel.add(sendButton, BorderLayout.WEST);
+
+        exitButton = new JButton("Exit");
+        exitButton.addActionListener(e -> {
+            String departureMessage = "User has left the chat";
+            client.sendMessage(departureMessage);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException interruptedException) {
+                Thread.currentThread().interrupt();
             }
+            System.exit(0);
         });
-        add(textField, BorderLayout.SOUTH);
+        buttonPanel.add(exitButton, BorderLayout.EAST);
+
+        lowerPane.add(buttonPanel, BorderLayout.EAST);
+        add(lowerPane, BorderLayout.SOUTH);
 
         try {
-            client = new Client("localhost", 3000);
-            client.init();
+//            this.client = new Client("localhost", 3000, this::onMessageReceived);
+            this.client = new ChatClient(hostname, serverPort, this::onMessageReceived);
+            client.startClient();
         } catch (Exception e) {
             e.printStackTrace();
             JOptionPane.showMessageDialog(this, "Error: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             System.exit(1);
         }
+    }
+
+    private void onMessageReceived(String message) {
+        SwingUtilities.invokeLater(() -> messageArea.append(message + "\n"));
     }
 
     public static void main(String[] args) {
