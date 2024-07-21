b55b3d5beba5a773e814b30639d8a1c1d2d8cf6e
Mike Kostenko <vopros.m@gmail.com>
2024-07-21 11:29:25 +0100

    Add client test

--- server/src/test/java/com/company/server/ClientTest.java
+++ server/src/test/java/com/company/server/ClientTest.java
@@ -8,8 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @author Mike Kostenko on 01/03/2024
  */
 public class ClientTest {
+
     @Test
-    public void testClientApplication() {
-        assertEquals(1, 1);
+    public void testUserCanBeAddedToClient() {
+        Client client = new Client("localhost", 3000);
+        client.setChatUser(new ChatUser(null, "Mike", null));
+        assertEquals("Mike", client.getClientName());
     }
 }
